36c16da8e2a1d13f435623b34786ecb8d9e764c0
Markel Ramiro <gernika2002@gmail.com>
2024-06-12 13:03:31 +0200

    First change on HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div>
-      <h1>Welcome to Markel's Portfolio</h1>
+      <h1>Welcome to Markel&apos;s Portfolio</h1> 
       <p>A showcase of my projects and skills in Big Data, Backend Development, and Cloud Infrastructure.</p>
     </div>
     </main>
